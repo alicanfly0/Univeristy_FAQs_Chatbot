9f47b83bfb2f61e24473ca1cdb54db2155df8827
Ali Abbasi <aliwaseemabbasi@gmail.com>
2025-07-12 20:08:00 +0500

    Loaded intents.json and initialized lemmatizer

--- bot.py
+++ bot.py
@@ -2,3 +2,9 @@ import json
 import random
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
+
+# Load intents
+with open("intents.json", "r") as f:
+    data = json.load(f)
+
+lemmatizer = WordNetLemmatizer()
